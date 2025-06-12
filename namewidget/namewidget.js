d584c03f89978c5ed76dc37453a414b9ef75f2e5
JoshMcleods <160990042+JoshMcleods@users.noreply.github.com>
2025-06-12 14:30:44 +0000

    started namewidget

---
+++ namewidget/namewidget.js
@@ -0,0 +1,18 @@
+import { LitElement, html, css } from 'https://unpkg.com/lit?module';
+
+class NameWidget extends LitElement {
+    static styles = css` {
+  
+  
+  }
+  `;
+
+    render() {
+
+        return html`
+  
+  `
+    }
+}
+
+customElements.define('name-widget', NameWidget);
